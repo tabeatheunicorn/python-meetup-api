06f0901f58738749cdda1eec3b5157eab837750a
tabeatheunicorn <Tabea.Roethemeyer@rub.de>
2023-05-16 14:46:01 +0200

    Step 1.2: Add response models

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
+from fastapi.responses import HTMLResponse
+from typing import List
 from fastapi import FastAPI
+
+from .models import Device, MeasurementMeta, MeasurementData
 
 app = FastAPI()
 
@@ -8,19 +12,20 @@ def root():
     return {"message": "Hello World"}
 
 
-@app.get("/devices")
+@app.get("/devices", response_model=List[Device])
 def get_all_devices():
-    ...
+    return []
     
-@app.get("/device/{device_id}/measurements")
+@app.get("/device/{device_id}/measurements", response_model=List[MeasurementMeta])
 def get_measurements_for_device(device_id):
-    ...
+    return []
     
-@app.get("/measurements/{measurement_id}")
+@app.get("/measurements/{measurement_id}", response_model=MeasurementData)
 def get_data_from_measurement(measurement_id):
-    ...
+   ...
     
 
-@app.get("/measurements/{measurement_id}/graph")
+#https://fastapi.tiangolo.com/advanced/custom-response/
+@app.get("/measurements/{measurement_id}/graph", response_class=HTMLResponse)
 def get_graph_from_measurement(measurement_id):
     ...
