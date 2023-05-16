145ef6972ec5ea008ade1074c14e90506bda309d
tabeatheunicorn <Tabea.Roethemeyer@rub.de>
2023-05-16 14:33:03 +0200

    Step 1: create inital routes

--- src/main.py
+++ src/main.py
@@ -6,3 +6,21 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "Hello World"}
+
+
+@app.get("/devices")
+def get_all_devices():
+    ...
+    
+@app.get("/device/{device_id}/measurements")
+def get_measurements_for_device(device_id):
+    ...
+    
+@app.get("/measurements/{measurement_id}")
+def get_data_from_measurement(measurement_id):
+    ...
+    
+
+@app.get("/measurements/{measurement_id}/graph")
+def get_graph_from_measurement(measurement_id):
+    ...
